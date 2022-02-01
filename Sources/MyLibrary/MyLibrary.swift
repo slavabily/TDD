60a2600301f79d1e9bf683d6cf7cb2e89490c551
slava bily <slavabily@me.com>
2022-02-01 18:24:39 +0200

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,5 @@
+public extension Bool {
+    init(bit: Int) {
+        self = bit == 1
+    }
+}
---
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+import MyLibrary
+
+final class MyLibraryTests: XCTestCase {
+    func test_Bool_init_bit() {
+     _ = Bool(bit: 1)
+    }
+}
